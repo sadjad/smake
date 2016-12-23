04a828000c888032a8dd9b24ea5415088353622a
Sadjad Fouladi <sfouladi@gmail.com>
2016-12-22 21:58:55 -0800

    Read make -nd output and generate the graph.

--- makefile.py
+++ makefile.py
@@ -2,10 +2,15 @@ import re
 import itertools
 
 class Makefile:
-    class Line:
+    class LineTypes:
+        TargetStart = 1
+        TargetEnd = 2
+        PruningFile = 3
+
     class Strings:
         TARGET_START = r"Considering target file '(.*)'\."
         TARGET_END = r"Finished prerequisites of target file '(.*)'\."
+        PRUNING_FILE = r"Pruning file '(.*)'\."
 
         @staticmethod
         def level(line):
@@ -16,6 +21,16 @@ class Makefile:
             search = re.search(cls.TARGET_START, line)
 
             if search:
-                return search.group(1)
+                return search.group(1), Makefile.LineTypes.TargetStart
 
-            return None
+            search = re.search(cls.TARGET_END, line)
+
+            if search:
+                return search.group(1), Makefile.LineTypes.TargetEnd
+
+            search = re.search(cls.PRUNING_FILE, line)
+
+            if search:
+                return search.group(1), Makefile.LineTypes.PruningFile
+
+            return None, None
--- smake.py
+++ smake.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import re
 import os
 import sys
+import pprint
 
 from makefile import Makefile
 
@@ -23,7 +24,7 @@ def build_graph(data):
         line = line.rstrip()
 
         if not started:
-            target_name = Makefile.Strings.target(line)
+            target_name, _ = Makefile.Strings.target(line)
             if target_name and target_name != "Makefile":
                 started = True
                 lqueue = [target_name]
@@ -32,14 +33,28 @@ def build_graph(data):
 
             continue
 
-        
+        target_name, line_type = Makefile.Strings.target(line)
+
+        if not target_name: continue
+
+        if line_type == Makefile.LineTypes.TargetStart:
+            if target_name not in graph:
+                graph[target_name] = []
+                nodes[target_name] = Node()
+
+            graph[target_name] += [lqueue[-1]]
+            lqueue.append(target_name)
+        elif line_type == Makefile.LineTypes.TargetEnd:
+            lqueue.pop()
+        elif line_type == Makefile.LineTypes.PruningFile:
+            graph[target_name] += [lqueue[-1]]
 
     return (graph, nodes)
 
 def main():
     graph, nodes = build_graph(sys.stdin)
-    print(graph)
-    print(nodes)
+    pprint.pprint(graph)
+    pprint.pprint(nodes)
 
 if __name__ == '__main__':
     main()
