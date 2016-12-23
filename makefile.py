2f1228a99e3ce5b1b45995a5a65e1cbe59c6936a
Sadjad Fouladi <sfouladi@gmail.com>
2016-12-22 22:24:13 -0800

    Include the commands for making a target in graph.

--- makefile.py
+++ makefile.py
@@ -2,35 +2,33 @@ import re
 import itertools
 
 class Makefile:
-    class LineTypes:
+    class Lines:
         TargetStart = 1
         TargetEnd = 2
         PruningFile = 3
+        MustRemake = 4
+        RemakeDone = 5
 
-    class Strings:
-        TARGET_START = r"Considering target file '(.*)'\."
-        TARGET_END = r"Finished prerequisites of target file '(.*)'\."
-        PRUNING_FILE = r"Pruning file '(.*)'\."
+        TARGET_START = (r"Considering target file '(.*)'\.", TargetStart)
+        TARGET_END = (r"Finished prerequisites of target file '(.*)'\.", TargetEnd)
+        PRUNING_FILE = (r"Pruning file '(.*)'\.", PruningFile)
+        MUST_REMAKE = (r"Must remake target '(.*)'\.", MustRemake)
+        REMAKE_DONE = (r"Successfully remade target file '(.*)'\.", RemakeDone)
 
-        @staticmethod
-        def level(line):
-            return sum([1 for _ in itertools.takewhile(str.isspace, line)])
+        All = (
+            TARGET_START, TARGET_END, PRUNING_FILE, MUST_REMAKE, REMAKE_DONE,
+        )
 
-        @classmethod
-        def target(cls, line):
-            search = re.search(cls.TARGET_START, line)
+    @staticmethod
+    def level(line):
+        return sum([1 for _ in itertools.takewhile(str.isspace, line)])
 
-            if search:
-                return search.group(1), Makefile.LineTypes.TargetStart
-
-            search = re.search(cls.TARGET_END, line)
-
-            if search:
-                return search.group(1), Makefile.LineTypes.TargetEnd
-
-            search = re.search(cls.PRUNING_FILE, line)
+    @classmethod
+    def identify(cls, line):
+        for pattern, line_type in Makefile.Lines.All:
+            search = re.search(pattern, line)
 
             if search:
-                return search.group(1), Makefile.LineTypes.PruningFile
+                return search.group(1), line_type
 
-            return None, None
+        return None, None
--- smake.py
+++ smake.py
@@ -10,11 +10,16 @@ import pprint
 from makefile import Makefile
 
 class Node:
-    def __init__(self):
-        pass
+    def __init__(self, target_name):
+        self.target_name = target_name
+        self.commands_list = []
+
+    def __repr__(self):
+        return "{}".format("\n".join(self.commands_list)) or "<none>"
 
 def build_graph(data):
     started = False
+    read_commands = (False, None) # (command reading mode, target name)
     graph = {}
     nodes = {}
 
@@ -23,31 +28,45 @@ def build_graph(data):
     for line in data:
         line = line.rstrip()
 
-        if not started:
-            target_name, _ = Makefile.Strings.target(line)
-            if target_name and target_name != "Makefile":
-                started = True
-                lqueue = [target_name]
-                graph[target_name] = [] # create the root!
-                nodes[target_name] = Node()
+        if read_commands[0]:
+            _, line_type = Makefile.identify(line)
+
+            if line_type == Makefile.Lines.RemakeDone:
+                read_commands = (False, None)
+            elif line_type:
+                raise Exception("unexpected line type")
+            else:
+                nodes[read_commands[1]].commands_list += [line]
 
             continue
 
-        target_name, line_type = Makefile.Strings.target(line)
+        if not started:
+            target_name, _ = Makefile.identify(line)
+            if target_name and target_name != "Makefile":
+                started = True
+                lqueue = [target_name]
+                graph[target_name] = ['(root)'] # create the root
+                nodes[target_name] = Node(target_name)
+
+            continue
+
+        target_name, line_type = Makefile.identify(line)
 
         if not target_name: continue
 
-        if line_type == Makefile.LineTypes.TargetStart:
+        if line_type == Makefile.Lines.TargetStart:
             if target_name not in graph:
                 graph[target_name] = []
-                nodes[target_name] = Node()
+                nodes[target_name] = Node(target_name)
 
             graph[target_name] += [lqueue[-1]]
             lqueue.append(target_name)
-        elif line_type == Makefile.LineTypes.TargetEnd:
+        elif line_type == Makefile.Lines.TargetEnd:
             lqueue.pop()
-        elif line_type == Makefile.LineTypes.PruningFile:
+        elif line_type == Makefile.Lines.PruningFile:
             graph[target_name] += [lqueue[-1]]
+        elif line_type == Makefile.Lines.MustRemake:
+            read_commands = (True, target_name)
 
     return (graph, nodes)
 
